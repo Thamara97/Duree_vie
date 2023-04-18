5293942abf7037a499e0033e9f1b80f5faed7d5a
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2023-04-18 10:32:01 +0200

    Update Question_1.R

--- Projet_Estimation/Question_1.R
+++ Projet_Estimation/Question_1.R
@@ -20,22 +20,25 @@ for (theta in seq(0, 5, by = 0.5)) {
     X <- rdoublex(n, mu = 0, lambda = 1) # échantillon X
     Y <- rdoublex(m, mu = theta, lambda = 1) # échantillon Y
       
-    # Test de Student
-    Stud_test <- t.test(X, Y, alternative = "less")
-    if (Stud_test$p.value < 0.05) {nb_ps <- nb_ps + 1} 
+    if (shapiro.test(X)$p.value > 0.05 | shapiro.test(Y)$p.value > 0.05) {
+      # Test de Student
+      Stud_test <- t.test(X, Y, alternative = "less")
+      if (Stud_test$p.value < 0.05) {nb_ps <- nb_ps + 1} 
+        
+      # Test de Wilcoxon
+      Wil_test <- wilcox.test(X, Y, alternative = "less")
+      if (Wil_test$p.value < 0.05) {nb_pw <- nb_pw + 1}
       
-    # Test de Wilcoxon
-    Wil_test <- wilcox.test(X, Y, alternative = "less")
-    if (Wil_test$p.value < 0.05) {nb_pw <- nb_pw + 1}
+      # Test de Kolmogorov-Smirnov
+      KS_test <- ks.test(X, Y, alternative = "two.sided")
+      if (KS_test$p.value < 0.05) {nb_pks <- nb_pks + 1}
+    }
     
-    # Test de Kolmogorov-Smirnov
-    KS_test <- ks.test(X, Y, alternative = "two.sided")
-    if (KS_test$p.value < 0.05) {nb_pks <- nb_pks + 1}
   }
   
-  ps[j] <- nb_ps/10000 # proportion de p-value Student
-  pw[j] <- nb_pw/10000 # proportion de p-value Wilcoxon
-  pks[j] <- nb_pks/10000 # proportion de p-value Kolmogorov
+  ps[j] <- nb_ps/10000 # 
+  pw[j] <- nb_pw/10000
+  pks[j] <- nb_pks/10000
 }
 
 plot(seq(0, 5, by = 0.5), ps, type = "o", col = "blue", xlab = "", ylab = "Puissance")
